2f24e70ae3ce3e1e86dbb917fd5106149618fd59
Dan Negrusa <negrusadan95@gmail.com>
2023-05-18 20:24:37 +0100

    assign struct as another struct's property

--- structs/main.go
+++ structs/main.go
@@ -2,9 +2,15 @@ package main
 
 import "fmt"
 
+type contactInfo struct {
+	email   string
+	zipCode int
+}
+
 type person struct {
 	firstName string
 	lastName  string
+	contact   contactInfo
 }
 
 func main() {
@@ -13,6 +19,24 @@ func main() {
 	// alex := person{"Alex", "Anderson"}
 
 	// we can also pass them by property name, which is much safer
-	alex := person{firstName: "Alex", lastName: "Anderson"}
-	fmt.Println(alex)
+	// alex := person{firstName: "Alex", lastName: "Anderson"}
+	// fmt.Println(alex)
+
+	// or initializing an empty person with "zero values" and later populate them
+	// var alex person
+	// alex.firstName = "Alex"
+	// alex.lastName = "Anderson"
+	// fmt.Println(alex)
+	// fmt.Printf("%+v", alex)
+
+	jim := person{
+		firstName: "Jim",
+		lastName:  "Party",
+		contact: contactInfo{
+			email:   "jimparty@gmail.com",
+			zipCode: 94000,
+		},
+	}
+
+	fmt.Printf("%+v", jim)
 }
