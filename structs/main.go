f684b91a8d0f074e823bcc4f6763740543dca4b7
Dan Negrusa <negrusadan95@gmail.com>
2023-05-20 13:47:07 +0100

    add comment

--- structs/main.go
+++ structs/main.go
@@ -25,7 +25,10 @@ func main() {
 	// jim.updateName("Jimmy")
 	// jim.print()
 
+	// the & in front of the variable means we're accessing
+	// the memory address of the value
 	jimPointer := &jim
+	fmt.Println(jimPointer)
 	jimPointer.updateName("jimmy")
 	jim.print()
 
@@ -34,6 +37,17 @@ func main() {
 // go is a "pass by value" language as opposed to javascript which is "pass by reference"
 // this means, when we pass a struct to a function, go is going to clone the value of said
 // struct into a new memory in address and use that instead. Think of immutability
+// func (p person) updateName(newFirstName string) {
+// 	p.firstName = newFirstName
+// }
+
+// the * in front of the variable means we're accessing the
+// value this memory address points to. Since the receiver here
+// is a pointer (jimPointer), we need to dereference it by using
+// *pointerToPerson in order to get the value at that address.
+// Also, *person is a TYPE, not an actual dereferenced value.
+// It only means that the pointerToPerson has to be of type
+// pointer to person
 func (pointerToPerson *person) updateName(newFirstName string) {
 	(*pointerToPerson).firstName = newFirstName
 }
