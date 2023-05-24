cb3316900b9a56383f744471705bfd6f775646c1
Dan Negrusa <negrusadan95@gmail.com>
2023-05-24 20:04:21 +0100

    create a slice that will e passed to the Reader function and populated with the response from the API

--- http/main.go
+++ http/main.go
@@ -14,5 +14,14 @@ func main() {
 		os.Exit(1)
 	}
 
-	fmt.Println(*resp)
+	// Make() will create a byte slcie of size 99999.
+	// A slice can grow and shrink but we can also
+	// initialise it with n elements
+	bs := make([]byte, 99999)
+
+	// we pass the empty slice to Read. Then, Read will
+	// mutate that slice and put the response data in it.
+	resp.Body.Read(bs)
+
+	fmt.Println(string(bs))
 }
