b13574f0c3c8bb8585fb7a42df10f4e2702d826c
Dan Negrusa <negrusadan95@gmail.com>
2023-05-25 19:50:57 +0100

    write a logWriter type that implements the Writer interface

--- http/main.go
+++ http/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"io"
 	"net/http"
 	"os"
 )
@@ -17,11 +18,27 @@ func main() {
 	// Make() will create a byte slcie of size 99999.
 	// A slice can grow and shrink but we can also
 	// initialise it with n elements
-	bs := make([]byte, 99999)
+	// bs := make([]byte, 99999)
 
 	// we pass the empty slice to Read. Then, Read will
 	// mutate that slice and put the response data in it.
-	resp.Body.Read(bs)
+	// resp.Body.Read(bs)
+	// fmt.Println(string(bs))
 
+	// faster alternative to all the code above
+	io.Copy(os.Stdout, resp.Body)
+
+	lw := logWriter{}
+	io.Copy(lw, resp.Body)
+
+}
+
+type logWriter struct{}
+
+func (logWriter) Write(bs []byte) (int, error) {
 	fmt.Println(string(bs))
+
+	// the Writer function needs to return the number of processed bytes as the first argument
+	// and the error or nil as the second argument
+	return len(bs), nil
 }
